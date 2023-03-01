1796a9bd7a665bd81ff6e87b3edd225c092fd2d5
anbuganesh kanan <v-anbuganesh.kanan@rksv.in>
2023-03-01 13:12:56 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import logoImg from './assets/images/nm_logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg-trans'></div>
+    </div>
+  );
+}
+
+export default App;
